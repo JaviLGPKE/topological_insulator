e204731372647db2de1704f6c6fe2a6c16d886e0
JaviLGPKE <javier.lgpke@gmail.com>
2025-04-04 12:09:30 +0100

    Spin Orbit Coupling - Implementation

--- tests/tight_binding/real_space.py
+++ tests/tight_binding/real_space.py
@@ -10,9 +10,9 @@ sub_2 = ["B", "A"]
 for sub_label, nn_label in zip(sub_1, sub_2):
     parser = getattr(problem.cell_parser.eigenvalues, sub_label).value
     parser["nn_hopping"][nn_label]["t_ss_sigma"] = -1
-    # parser["nn_hopping"][nn_label]["t_sp_sigma"] = 1
-    # parser["nn_hopping"][nn_label]["t_pp_sigma"] = 1
-    # parser["nn_hopping"][nn_label]["t_pp_pi"] = -1.4
+    parser["nn_hopping"][nn_label]["t_sp_sigma"] = 1
+    parser["nn_hopping"][nn_label]["t_pp_sigma"] = 1
+    parser["nn_hopping"][nn_label]["t_pp_pi"] = -1.4
     parser["SO_coupling"][sub_label]["lambda_pp"] = 0.3
 
 problem.setup(
